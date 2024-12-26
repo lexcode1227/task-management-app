2302b2e94881596cdafd51fa11ff6dd28dfe91a3
Henry Agustin <henryagustin297@gmail.com>
2024-12-26 15:42:34 -0600

    feat: add codegen tool to generate type from the api

---
+++ src/graphql/graphql.ts
@@ -0,0 +1,179 @@
+/* eslint-disable */
+import { DocumentTypeDecoration } from '@graphql-typed-document-node/core';
+export type Maybe<T> = T | null;
+export type InputMaybe<T> = Maybe<T>;
+export type Exact<T extends { [key: string]: unknown }> = { [K in keyof T]: T[K] };
+export type MakeOptional<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]?: Maybe<T[SubKey]> };
+export type MakeMaybe<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]: Maybe<T[SubKey]> };
+export type MakeEmpty<T extends { [key: string]: unknown }, K extends keyof T> = { [_ in K]?: never };
+export type Incremental<T> = T | { [P in keyof T]?: P extends ' $fragmentName' | '__typename' ? T[P] : never };
+/** All built-in and custom scalars, mapped to their actual values */
+export type Scalars = {
+  ID: { input: string; output: string; }
+  String: { input: string; output: string; }
+  Boolean: { input: boolean; output: boolean; }
+  Int: { input: number; output: number; }
+  Float: { input: number; output: number; }
+  /** A date-time string at UTC, such as 2019-12-03T09:54:33Z, compliant with the date-time format. */
+  DateTime: { input: any; output: any; }
+};
+
+export type CreateTaskInput = {
+  assigneeId?: InputMaybe<Scalars['String']['input']>;
+  dueDate: Scalars['DateTime']['input'];
+  name: Scalars['String']['input'];
+  pointEstimate: PointEstimate;
+  status: Status;
+  tags: Array<TaskTag>;
+};
+
+export type DeleteTaskInput = {
+  id: Scalars['String']['input'];
+};
+
+export type FilterTaskInput = {
+  assigneeId?: InputMaybe<Scalars['String']['input']>;
+  dueDate?: InputMaybe<Scalars['DateTime']['input']>;
+  name?: InputMaybe<Scalars['String']['input']>;
+  ownerId?: InputMaybe<Scalars['String']['input']>;
+  pointEstimate?: InputMaybe<PointEstimate>;
+  status?: InputMaybe<Status>;
+  tags?: InputMaybe<Array<TaskTag>>;
+};
+
+export type Mutation = {
+  __typename?: 'Mutation';
+  createTask: Task;
+  deleteTask: Task;
+  updateTask: Task;
+};
+
+
+export type MutationCreateTaskArgs = {
+  input: CreateTaskInput;
+};
+
+
+export type MutationDeleteTaskArgs = {
+  input: DeleteTaskInput;
+};
+
+
+export type MutationUpdateTaskArgs = {
+  input: UpdateTaskInput;
+};
+
+/** Estimate point for a task */
+export enum PointEstimate {
+  Eight = 'EIGHT',
+  Four = 'FOUR',
+  One = 'ONE',
+  Two = 'TWO',
+  Zero = 'ZERO'
+}
+
+export type Query = {
+  __typename?: 'Query';
+  profile: User;
+  tasks: Array<Task>;
+  users: Array<User>;
+};
+
+
+export type QueryTasksArgs = {
+  input: FilterTaskInput;
+};
+
+/** Status for Task */
+export enum Status {
+  Backlog = 'BACKLOG',
+  Cancelled = 'CANCELLED',
+  Done = 'DONE',
+  InProgress = 'IN_PROGRESS',
+  Todo = 'TODO'
+}
+
+export type Task = {
+  __typename?: 'Task';
+  assignee?: Maybe<User>;
+  createdAt: Scalars['DateTime']['output'];
+  creator: User;
+  dueDate: Scalars['DateTime']['output'];
+  id: Scalars['ID']['output'];
+  name: Scalars['String']['output'];
+  pointEstimate: PointEstimate;
+  position: Scalars['Float']['output'];
+  status: Status;
+  tags: Array<TaskTag>;
+};
+
+/** Enum for tags for tasks */
+export enum TaskTag {
+  Android = 'ANDROID',
+  Ios = 'IOS',
+  NodeJs = 'NODE_JS',
+  Rails = 'RAILS',
+  React = 'REACT'
+}
+
+export type UpdateTaskInput = {
+  assigneeId?: InputMaybe<Scalars['String']['input']>;
+  dueDate?: InputMaybe<Scalars['DateTime']['input']>;
+  id: Scalars['String']['input'];
+  name?: InputMaybe<Scalars['String']['input']>;
+  pointEstimate?: InputMaybe<PointEstimate>;
+  position?: InputMaybe<Scalars['Float']['input']>;
+  status?: InputMaybe<Status>;
+  tags?: InputMaybe<Array<TaskTag>>;
+};
+
+export type User = {
+  __typename?: 'User';
+  avatar?: Maybe<Scalars['String']['output']>;
+  createdAt: Scalars['DateTime']['output'];
+  email: Scalars['String']['output'];
+  fullName: Scalars['String']['output'];
+  id: Scalars['ID']['output'];
+  type: UserType;
+  updatedAt: Scalars['DateTime']['output'];
+};
+
+/** Type of the User */
+export enum UserType {
+  Admin = 'ADMIN',
+  Candidate = 'CANDIDATE'
+}
+
+export type ProfileQueryVariables = Exact<{ [key: string]: never; }>;
+
+
+export type ProfileQuery = { __typename?: 'Query', profile: { __typename?: 'User', id: string, fullName: string, email: string, avatar?: string | null, createdAt: any, updatedAt: any, type: UserType } };
+
+export class TypedDocumentString<TResult, TVariables>
+  extends String
+  implements DocumentTypeDecoration<TResult, TVariables>
+{
+  __apiType?: DocumentTypeDecoration<TResult, TVariables>['__apiType'];
+
+  constructor(private value: string, public __meta__?: Record<string, any> | undefined) {
+    super(value);
+  }
+
+  toString(): string & DocumentTypeDecoration<TResult, TVariables> {
+    return this.value;
+  }
+}
+
+export const ProfileDocument = new TypedDocumentString(`
+    query Profile {
+  profile {
+    id
+    fullName
+    email
+    avatar
+    createdAt
+    updatedAt
+    type
+  }
+}
+    `) as unknown as TypedDocumentString<ProfileQuery, ProfileQueryVariables>;
