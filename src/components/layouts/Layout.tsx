2137148effc83320831b1e748d35153a3d8eb5ab
Henry Agustin <henryagustin297@gmail.com>
2024-12-31 09:36:23 -0600

    refactor: optimize layout component structure and readability

--- src/components/layouts/Layout.tsx
+++ src/components/layouts/Layout.tsx
@@ -6,17 +6,19 @@ interface Props {
   children: React.ReactNode;
 }
 
-const Layout: React.FunctionComponent<Props> = (props: Props) => {
-  return <main className="flex gap-8 h-screen w-full bg-color_neutral_5 p-8 pb-0">
-    <Sidebar/>
-    <section className="w-full">
-        <Searchbar/>
-        <Topbar/>
-        <section className="w-full mt-4">
-            {props.children}
-        </section>
-    </section>
-  </main>;
+const Layout = ({children}: Props) => {
+  return (
+    <main className="flex gap-8 h-screen w-full bg-color_neutral_5 p-8 pb-0">
+      <Sidebar/>
+      <section className="w-full">
+          <Searchbar/>
+          <Topbar/>
+          <section className="w-full mt-4">
+              {children}
+          </section>
+      </section>
+    </main>
+  );
 };
 
 export default Layout;
